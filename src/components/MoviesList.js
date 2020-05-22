5e39ccb872019a42f6c750d12dc56cf7a7bc49c6
Jose Angel Flores <52080294+AngelFFlores@users.noreply.github.com>
2020-05-22 10:37:03 -0500

    clases nuevas y cmabios l&f

---
+++ src/components/MoviesList.js
@@ -0,0 +1,28 @@
+import React, { Component } from "react";
+import PropTypes from "prop-types";
+import { Movie } from "./Movie";
+
+export class MoviesList extends Component {
+  static propTypes = {
+    movies: PropTypes.array,
+  };
+
+  render() {
+    const { movies } = this.props;
+    return (
+      <div className="MoviesList">
+        { movies.map((movie) => {
+          return (
+              <div className =" MoviesList-item" key={movie.imdbID}s>
+            <Movie
+              title={movie.Title}
+              year={movie.year}
+              poster={movie.Poster}
+            />
+            </div>
+          )
+        })}
+      </div>
+    );
+  }
+}
