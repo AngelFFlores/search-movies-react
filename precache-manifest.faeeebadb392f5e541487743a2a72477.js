c6d70087a6092aa152e5a22f6eafe2d08bee1502
Jose Angel Flores <52080294+AngelFFlores@users.noreply.github.com>
2020-05-22 13:03:00 -0500

    Updates

---
+++ precache-manifest.faeeebadb392f5e541487743a2a72477.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7b04e97435b27f246685e5ae1d60488f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1701de292391b6cc0e4b",
+    "url": "/static/css/2.c3343ac3.chunk.css"
+  },
+  {
+    "revision": "4fb0f687fdfe45111098",
+    "url": "/static/css/main.f999c17a.chunk.css"
+  },
+  {
+    "revision": "1701de292391b6cc0e4b",
+    "url": "/static/js/2.c1d449df.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.c1d449df.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4fb0f687fdfe45111098",
+    "url": "/static/js/main.4916474e.chunk.js"
+  },
+  {
+    "revision": "906fbc7443f51b21e6d6",
+    "url": "/static/js/runtime-main.2a3bf836.js"
+  }
+]);
